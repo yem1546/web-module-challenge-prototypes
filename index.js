e2097aa45ad942e7a73381a7757312367a4edff5
yem1546 <87500366+yem1546@users.noreply.github.com>
2022-08-13 15:18:46 -0400

    Completed first challenge

--- index.js
+++ index.js
@@ -15,11 +15,23 @@
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
-
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+  this.stomach = [];
 }
 
-
+Person.prototype.eat = function(someFood){
+  if (this.stomach.length < 10){
+    this.stomach.push(someFood);
+  }
+}
+Person.prototype.poop = function(){
+  this.stomach = []
+}
+Person.prototype.toString = function(){
+  return `${this.name}, ${this.age}`
+}
 /*
   TASK 2
     - Write a Car constructor that initializes `model` and `milesPerGallon` from arguments.
